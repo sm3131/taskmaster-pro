657eff1e0ddfde89134a098e95767f4743a139c7
sm3131 <sm2683@nau.edu>
2021-11-22 01:41:15 -0700

    added drag and drop trash method, made draggable work on mobile devices

--- assets/js/script.js
+++ assets/js/script.js
@@ -192,7 +192,7 @@ $(".card .list-group").sortable({
   out: function (event) {
     console.log("out", event.target);
   },
-  update: function () {
+  update: function() {
     // array to store the task data in
     var tempArr = [];
 
@@ -215,15 +215,30 @@ $(".card .list-group").sortable({
       });
     });
 
-            // trim down list's ID to match object property
-            var arrName = $(this)
-            .attr("id")
-            .replace("list-", "");
-    
-          // update array on tasks object and save
-          tasks[arrName] = tempArr;
-          saveTasks();
+    // trim down list's ID to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
     console.log(tempArr);
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function (event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function (event, ui) {
+    console.log("over");
+  },
+  out: function (event, ui) {
+    console.log("out");
   }
 });
 
